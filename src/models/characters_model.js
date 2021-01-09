34d8ebbc8d0afde3366cb814da7c198698226623
João Vitor <joaovitor3592@gmail.com>
2021-01-08 21:51:27 -0300

    Update characters_model.js
    adding update query

--- src/models/characters_model.js
+++ src/models/characters_model.js
@@ -9,11 +9,45 @@ exports.get_characters = async () => {
     console.log("DB ERROR: ", err);
   }
 }
+exports.get_character = async (character) => {
+  try{
+    if(isNaN(character)){
+      return await db
+      .select(['characters.id', 'name'])
+      .from('characters')
+      .where({ slug: character });
+    }else{
+      return await db
+      .select(['characters.id', 'name'])
+      .from('characters')
+      .where({ id: character });
+    }
+  }catch(err){
+    console.log("DB ERROR: ", err);
+  }
+}
 exports.new_character = async (data) => {
   try{
     return await db
     .insert(data)
     .into( 'characters');
+  }catch(err){
+    console.log("DB ERROR: ", err);
+  }
+}
+exports.update_character = async (character, data) => {
+  try{
+    if(isNaN(character)){
+      return await db
+      .table('characters')
+      .update(data)
+      .where({ slug: character });
+    }else{
+      return await db
+      .table('characters')
+      .update(data)
+      .where({ id: character });
+    }
   }catch(err){
     console.log("DB ERROR: ", err);
   }
