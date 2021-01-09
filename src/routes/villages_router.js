42459c6ecc3ff465c7586369410c40e06e864fe0
João Vitor <joaovitor3592@gmail.com>
2021-01-08 21:49:06 -0300

    Update villages_router.js
    Adding update route

--- src/routes/villages_router.js
+++ src/routes/villages_router.js
@@ -4,6 +4,7 @@ const villages_controller = require('../controllers/villages_controller');
 
 router.get('/api/villages', villages_controller.get_villages);
 router.post('/api/village', villages_controller.new_village);
+router.put('/api/village', villages_controller.update_village);
 router.delete('/api/village', villages_controller.delete_village);
 
 module.exports = router;
