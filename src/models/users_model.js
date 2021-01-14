18d681edeea479180b2f027cd0357829de1a84f6
João Vitor <joaovitor3592@gmail.com>
2021-01-14 00:30:05 -0300

    Create users_model.js
    Addind the model for users

---
+++ src/models/users_model.js
@@ -0,0 +1,50 @@
+const db = require('../database/database');
+
+exports.get_users = async () => {
+  try{
+    return await db
+    .select(['user_id', 'user_name', 'user_email'])
+    .from('users');
+  }catch(err){
+    console.log("DB ERROR(table users): ", err);
+  }
+}
+exports.get_user_by_email = async (user_email) => {
+  try{
+    return await db
+    .select(['user_id', 'user_email'])
+    .from('users')
+    .where({ user_email });
+  }catch(err){
+    console.log("DB ERROR(table users): ", err);
+  }
+}
+exports.new_user = async (data) => {
+  try{
+    return await db
+    .insert(data)
+    .into('users');
+  }catch(err){
+    console.log("DB ERROR(table users): ", err);
+  }
+}
+exports.update_user = async (user_id, data) => {
+  try{
+    return await db
+    .table('users')
+    .update(data)
+    .where({ user_id });
+  }catch(err){
+    console.log("DB ERROR(table users): ", err);s
+  }
+}
+exports.delete_user = async (user_id) => {
+  try{
+    return await db
+    .table('users')
+    .where({ user_id })
+    .del();
+  }catch(err){
+    console.log("DB ERROR(table users): ", err);
+  }
+}
