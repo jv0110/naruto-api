0a0d760c0aa2754119a1ec04965c48e6cd51538a
João Vitor <joaovitor3592@gmail.com>
2021-01-16 02:12:02 -0300

    Update users_router.js
    Changin some route names

--- src/routes/users_router.js
+++ src/routes/users_router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const users_controller = require('../controllers/users_controller');
+const auth = require('../middlewares/auth');
 
-router.get('/api/users', users_controller.get_users);
+router.get('/api/users', auth, users_controller.get_users);
 router.post('/api/user', users_controller.new_user);
-router.delete('/api/user', users_controller.delete_user);
+router.post('/api/user/login', users_controller.login);
 router.put('/api/user', users_controller.update_user);
+router.delete('/api/user', users_controller.delete_user);
 
 module.exports = router;
